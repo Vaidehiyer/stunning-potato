31bcb159e5684c133fcfa889ae068d8a458ccd9a
Vaidehiyer <60912404+Vaidehiyer@users.noreply.github.com>
2020-02-12 09:30:45 +0530

    Add files via upload

---
+++ vai.js
@@ -0,0 +1,5 @@
+var sample1= [{Name: "Anju",class: 1,Age: 5, display: function(){log("Name"+"Class"+"Age")}},
+{Name: "Anjana",class: 2,Age: 6,display:function(){log("Name"+"Class"+"Age")}},
+{Name: "Vaidu",class: 3,Age: 7, display:function(){log("Name"+"Class"+"Age")}},
+{Name: "Lakshmi",class: 4,Age: 8, display:function(){log("Name"+"Class"+"Age")}}]
+
